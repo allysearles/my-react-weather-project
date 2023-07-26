ca84ac4fb1667c0b2f1715aa07e3639ffd2ae3b5
allysearles <alyssa.searles001@mymdc.net>
2023-07-26 17:23:06 -0400

    Added Footer:
    -Contains github link with open source comment.
    -Adjusted some footer CSS attributes.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
 	return (
 		<div className="App">
 			<h1 className="page-title">Local Weather Search</h1>
+			<footer>
+				This project was coded by Ally Searles and is{" "}
+				<a
+					href="https://github.com/allysearles/my-react-weather-project.git"
+					target="_blank"
+					rel="noreferrer"
+				>
+					open-sourced{" "}
+				</a>
+			</footer>
 		</div>
 	);
 }
