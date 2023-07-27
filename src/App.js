639677139dc187d3a3748cc4c3401f375e13d1bd
allysearles <alyssa.searles001@mymdc.net>
2023-07-27 16:27:38 -0400

    Added Bootstrap into project

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<h1 className="page-title">Local Weather Search</h1>
-			<footer>
-				This project was coded by Ally Searles and is{" "}
-				<a
-					href="https://github.com/allysearles/my-react-weather-project.git"
-					target="_blank"
-					rel="noreferrer"
-				>
-					open-sourced{" "}
-				</a>
-			</footer>
+			<div className="container">
+				<h1 className="page-title">Local Weather Search</h1>
+				<footer>
+					This project was coded by Ally Searles and is{" "}
+					<a
+						href="https://github.com/allysearles/my-react-weather-project.git"
+						target="_blank"
+						rel="noreferrer"
+					>
+						open-sourced{" "}
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
