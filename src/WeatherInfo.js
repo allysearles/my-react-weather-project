968d0bf2641b46aa68ce9e4cdaadb78649a1b29c
allysearles <alyssa.searles001@mymdc.net>
2023-07-30 01:29:37 -0400

    Added weather metrics into weather application.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
 	return (
@@ -24,12 +25,8 @@ export default function WeatherInfo(props) {
 								alt={props.data.description}
 							/>
 						</div>
-
 						<div className="float-left">
-							<span className="temperature">
-								{Math.round(props.data.temperature)}
-							</span>
-							<span className="units">°C | °F</span>
+							<WeatherTemperature celsius={props.data.icon} />
 						</div>
 					</div>
 				</div>
